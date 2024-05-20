c2e0b46d53182917f3afe4fdc6196dcbba45c7cd
Santosh Dahal <sirishsds@gmail.com>
2024-05-20 14:14:22 +0545

    minor change for cust_edit_details

--- edit_customer_details.php
+++ edit_customer_details.php
@@ -40,6 +40,9 @@
                 <label>Nominee Name: <input name="nominee_name" value="<?php echo $row['Nominee_name']; ?>"></label><br>
                 <label>Nominee Ac/no: <input name="nominee_ac_no" value="<?php echo $row['Nominee_ac_no']; ?>"></label><br>
                 <label>Email Id: <input name="email_id" value="<?php echo $row['Email_ID']; ?>"></label><br>
+                <label>Address: <input name="home_addr" value="<?php echo $row['Home_Addr']; ?>"></label><br>
+                <label>Date of Birth: <input type="date" name="dob" value="<?php echo $row['DOB']; ?>"></label><br>
+                <label>PAN: <input name="pan" value="<?php echo $row['PAN']; ?>"></label><br>
                 <input type="submit" name="submit_update" value="Update">
             </form>
         </div>
@@ -61,6 +64,9 @@
         $nominee_name = $_POST['nominee_name'];
         $nominee_ac_no = $_POST['nominee_ac_no'];
         $email_id = $_POST['email_id'];
+        $home_addr = $_POST['home_addr'];
+        $dob = $_POST['dob'];
+        $pan = $_POST['pan'];
 
         $update_sql = "UPDATE bank_customers SET 
             Username='$username',
@@ -69,7 +75,10 @@
             Mobile_no='$mobile_no',
             Nominee_name='$nominee_name',
             Nominee_ac_no='$nominee_ac_no',
-            Email_ID='$email_id'
+            Email_ID='$email_id',
+            Home_Addr='$home_addr',
+            DOB='$dob',
+            PAN='$pan'
             WHERE Customer_ID='$customer_id'";
 
         if($conn->query($update_sql) === TRUE){
