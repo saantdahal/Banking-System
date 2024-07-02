09e373df63bfab6ebe7c260505312930f83eb0b0
Santosh Dahal <about@santoshdahal.info.np>
2024-07-02 15:48:09 +0545

    Bug fix on create_cust file and UI change

--- create_cust.php
+++ create_cust.php
@@ -7,7 +7,6 @@ include "user_navbar.php";
 include "session_timeout.php";
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="customer_add_style.css">
